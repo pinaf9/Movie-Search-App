b68d55eef214474cac9111e2f8061ce655dea62a
Francheska Pina <pinaf9@gmail.com>
2023-01-25 01:19:21 -0500

    adding react movie-app project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SearchMovies from './components/SearchMovies';
+import './index.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className='title'>Movie Search App</h1>
+      <SearchMovies />
+    </div>
+  );
+}
+
+export default App;
